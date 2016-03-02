2fa50cbf8dab0db07a2599ae1f887f91aeee2d6c
Simon Hitzemann <github@simon.hitzemann.org>
2016-03-02 14:14:30 +0100

    Use tabs or spaces, but not both

--- service/client.cpp
+++ service/client.cpp
@@ -104,9 +104,9 @@ void Client::Run() {
 
         if (msg.type == MSG_ENABLED) {
             printf("1\n");
-	} else if (msg.type == MSG_DISABLED) {
+        } else if (msg.type == MSG_DISABLED) {
             printf("0\n");
-	} else {
+        } else {
             perror("Error");
             closeConnection(g_serverFd);
             exit(EXIT_FAILURE);
